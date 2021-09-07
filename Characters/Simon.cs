ea95e2518d666b68dd26bbe4337b51c535720cf0
Thomas Kent <the1krutz@gmail.com>
2021-09-06 23:54:09 -0700

    move obstacle course to separate scene added some more stairs to level 1 set 1 found a fun bug where if you're at the bottom of one set of stairs and the top of another (a very common occurance), then the controller can't figure out which staircase you're trying to enter. It'll pick one semi-randomly, and it's 50/50 whether you'll get the one you wanted XD

--- Characters/Simon.cs
+++ Characters/Simon.cs
@@ -1,4 +1,3 @@
-using System.Dynamic;
 using Godot;
 
 public class Simon : KinematicBody2D {
---
+++ Characters/StairsInfo.cs
@@ -0,0 +1,32 @@
+using Godot;
+
+public class StairsInfo {
+  // set
+  public Vector2 TopPoint { get; }
+  public Vector2 BottomPoint { get; }
+
+  // calculated
+  public Vector2 UpStairsDirection { get; }
+  public string AlternateUpInput { get; }
+  public string AlternateDownInput { get; }
+  public Vector2 UpScale { get; }
+  public Vector2 DownScale { get; }
+
+  public StairsInfo(Vector2 bottomPoint, Vector2 topPoint) {
+    TopPoint = topPoint;
+    BottomPoint = bottomPoint;
+
+    UpStairsDirection = BottomPoint.DirectionTo(TopPoint);
+    if (UpStairsDirection.x > 0) {
+      AlternateUpInput = "move_right";
+      AlternateDownInput = "move_left";
+      UpScale = new Vector2(-1, 1);
+      DownScale = new Vector2(1, 1);
+    } else {
+      AlternateUpInput = "move_left";
+      AlternateDownInput = "move_right";
+      UpScale = new Vector2(1, 1);
+      DownScale = new Vector2(-1, 1);
+    }
+  }
+}
