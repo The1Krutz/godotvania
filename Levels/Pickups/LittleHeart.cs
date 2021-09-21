eefd7b53c0b91bf13717ef8b6442fef13ef77442
Thomas Kent <the1krutz@gmail.com>
2021-09-21 13:53:30 -0700

    wip little heart physics falling

--- Levels/Pickups/LittleHeart.cs
+++ Levels/Pickups/LittleHeart.cs
@@ -2,9 +2,18 @@ using Godot;
 
 namespace Godotvania {
   public class LittleHeart : Area2D {
+    [Signal]
+    public delegate void CollectHeart(int count);
+
+    public override void _Ready() {
+      GD.Print("LittleHeart._Ready");
+    }
+
     public void OnAreaEntered(Area2D area) {
       // do the logic later
       GD.Print("LittleHeart.OnAreaEntered");
+      EmitSignal(nameof(CollectHeart), 1);
+      QueueFree();
     }
   }
 }
--- Levels/Whippables/GroundCandle.cs
+++ Levels/Whippables/GroundCandle.cs
@@ -2,9 +2,29 @@ using Godot;
 
 namespace Godotvania {
   public class GroundCandle : Node2D {
+    private PackedScene LittleHeartScene;
+    private AnimationPlayer animationPlayer;
+
+    public override void _Ready() {
+      LittleHeartScene = ResourceLoader.Load<PackedScene>("res://Levels/Pickups/LittleHeart.tscn");
+      animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+
+      animationPlayer.Play("idle");
+    }
+
     public void OnAreaEntered(Area2D area) {
-      // drop loot later
-      QueueFree();
+      GD.Print("GroundCandle.OnAreaEntered:start");
+      CallDeferred("DropLoot");
+      animationPlayer.Play("hit");
+      GD.Print("GroundCandle.OnAreaEntered:end");
+    }
+
+    public void DropLoot() {
+      GD.Print("GroundCandle.DropLoot:start");
+      Area2D newheart = (Area2D)LittleHeartScene.Instance();
+      newheart.Position = Position + new Vector2(0, -32);
+      GetParent().AddChild(newheart, true);
+      GD.Print("GroundCandle.DropLoot:end");
     }
   }
 }
