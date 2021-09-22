85169394b1c306391752708125c2c1735175cae0
Thomas Kent <the1krutz@gmail.com>
2021-09-22 00:39:29 -0700

    candles can now drop whatever, configurable from the editor
    fixd the floor colliders in some places to let trevor jump up partially through the bottom

--- Levels/Pickups/Pickup.cs
+++ Levels/Pickups/Pickup.cs
@@ -3,7 +3,10 @@ using Godot;
 namespace Godotvania {
   public class Pickup : Area2D {
     [Signal]
-    public delegate void CollectHeart(int count);
+    public delegate void CollectPickup(PickupType type);
+
+    [Export(hint: PropertyHint.Enum)]
+    public PickupType pickupType;
 
     public override void _Ready() {
       GD.Print("Pickup._Ready");
@@ -11,9 +14,15 @@ namespace Godotvania {
 
     public void OnAreaEntered(Area2D area) {
       // do the logic later
-      GD.Print("Pickup.OnAreaEntered");
-      EmitSignal(nameof(CollectHeart), 1);
+      GD.Print($"Pickup.OnAreaEntered: {pickupType}");
+      EmitSignal(nameof(CollectPickup), pickupType);
       GetParent().QueueFree();
     }
+  }
+
+  public enum PickupType {
+    LittleHeart,
+    BigHeart,
+    WhipUpgrade,
   }
 }
