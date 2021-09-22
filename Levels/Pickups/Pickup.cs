68b3aa066b9c8d8ffe690678f415c0cb2570d6a0
Thomas Kent <the1krutz@gmail.com>
2021-09-22 00:04:11 -0700

    GDV-34 add whip upgrade pickup

--- Levels/Pickups/Pickup.cs
+++ Levels/Pickups/Pickup.cs
@@ -1,17 +1,17 @@
 using Godot;
 
 namespace Godotvania {
-  public class LittleHeart : Area2D {
+  public class Pickup : Area2D {
     [Signal]
     public delegate void CollectHeart(int count);
 
     public override void _Ready() {
-      GD.Print("LittleHeart._Ready");
+      GD.Print("Pickup._Ready");
     }
 
     public void OnAreaEntered(Area2D area) {
       // do the logic later
-      GD.Print("LittleHeart.OnAreaEntered");
+      GD.Print("Pickup.OnAreaEntered");
       EmitSignal(nameof(CollectHeart), 1);
       GetParent().QueueFree();
     }
